3b593019e0e159177dafcdd7583af427d9539271
khayrutdinov_fr <khayrutdinov_fr@technology45.ru>
2023-03-05 16:10:37 +0300

    registration flow dialog

--- src/main3.py
+++ src/main3.py
@@ -7,20 +7,15 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import (CallbackQuery, InlineKeyboardButton,
                            InlineKeyboardMarkup, Message,ReplyKeyboardMarkup,KeyboardButton,ReplyKeyboardRemove)
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
-# полученный у @BotFather
+
 BOT_TOKEN = '5140163343:AAGaFLxhYrbFMaZ0aV0SRxHNgpJ4J3ld6EE'
-#База данных
 BotDB = BotDB('accountant.db')
-# Инициализируем хранилище (создаем экземпляр класса MemoryStorage)
 storage: MemoryStorage = MemoryStorage()
 
-# Создаем объекты бота и диспетчера
 bot: Bot = Bot(BOT_TOKEN)
 dp: Dispatcher = Dispatcher(bot, storage=storage)
 
 
-# Cоздаем класс, наследуемый от StatesGroup, для группы состояний нашей FSM
 class FSMFillForm(StatesGroup):
     # Создаем экземпляры класса State, последовательно
     # перечисляя возможные состояния, в которых будет находиться
@@ -34,9 +29,6 @@ class FSMFillForm(StatesGroup):
     fill_wish_news = State()  # Состояние ожидания выбора получать ли новости
 
 
-# Этот хэндлер будет срабатывать на команду /start
-# и предлагать перейти к заполнению анкеты,
-# отправив команду /fillform
 async def process_start_command(message: Message):
     if BotDB.user_exists(message.from_user.id):
         await message.answer(text='Вы уже зарегистрированы. Мы свяжемся с Вами как только подберем Вам попутчиков\n\n'
@@ -48,16 +40,12 @@ async def process_start_command(message: Message):
                               'отправьте нажмите сюда: /fillform')
 
 
-# Этот хэндлер будет срабатывать на команду /fillform
-# и переводить бота в состояние ожидания ввода имени
 async def process_fillform_command(message: Message):
     await message.answer(text='Пожалуйста, введите Ваше имя')
     # Устанавливаем состояние ожидания ввода имени
     await FSMFillForm.fill_name.set()
 
 
-# Этот хэндлер будет срабатывать, если во время ввода имени
-# будет введено что-то некорректное
 async def warning_not_name(message: Message):
     await message.answer(text='То, что вы отправили не похоже на имя\n\n'
                               'Пожалуйста, введите ваше имя\n\n'
@@ -65,44 +53,28 @@ async def warning_not_name(message: Message):
                               'отправьте команду /cancel')
 
 
-# Этот хэндлер будет срабатывать, если введено корректное имя
-# и переводить в состояние ожидания ввода возраста
 async def process_name_sent(message: Message, state: FSMContext):
-    # C помощью менеджера контекста сохраняем введенное имя
-    # в хранилище по ключу "name"
     name = message.text
     BotDB.add_name(message.from_user.id,name)
-    # Создаем объект инлайн-клавиатурs
     markup = InlineKeyboardMarkup()
-    # Создаем объекты инлайн-кнопок
-    driver_button = InlineKeyboardButton(text='Водитель',
-                                         callback_data='driver')
-    passanger_button = InlineKeyboardButton(text='Пассажир',
-                                            callback_data='passanger')
-    driverandpass_button = InlineKeyboardButton(text='И водитель и пассажир',
-                                                callback_data='driverandpass')
-    # Добавляем кнопки в клавиатуру (две в одном ряду и одну в другом)
+    driver_button = InlineKeyboardButton(text='Водитель',                                         callback_data='driver')
+    passanger_button = InlineKeyboardButton(text='Пассажир',                                            callback_data='passanger')
+    driverandpass_button = InlineKeyboardButton(text='И водитель и пассажир',                                                callback_data='driverandpass')
     markup.add(driver_button, passanger_button).add(driverandpass_button)
     await message.answer(
         text='Спасибо!\n\nА теперь Укажите в качестве кого Вы хотите зарегистроваться, Водитель или Пассажир',
         reply_markup=markup)
-    # Устанавливаем состояние ожидания выбора пола
     await FSMFillForm.fill_driverorpass.set()
 
 
-# Этот хэндлер будет срабатывать, если во время выбора статуса
-# будет введено/отправлено что-то некорректное
 async def warning_not_driverorpass(message: Message):
     await message.answer(text='Пожалуйста, пользуйтесь кнопками '
                               'при выборе пола\n\nЕсли вы хотите прервать '
                               'заполнение анкеты - отправьте команду /cancel')
 
 
-# Этот хэндлер будет срабатывать на нажатие кнопки при
-# выборе пола и переводить в состояние отправки фото
+
 async def process_driverorpass_press(callback: CallbackQuery, state: FSMContext):
-    # C помощью менеджера контекста сохраняем пол (callback.data нажатой
-    # кнопки) в хранилище, по ключу "driverorpass"
     driverorpass = callback.data
     BotDB.add_record_driverorpass(callback.from_user.id, driverorpass)
         # Удаляем сообщение с кнопками
@@ -173,37 +145,17 @@ async def warning_not_phone(message: Message):
 
 # Тут должна быть функция по получению номера телефона
 async def procces_phonenumber_sent(message: Message, state: FSMContext):
-# C помощью менеджера контекста сохраняем номер телефона
-# в хранилище по ключу "phone"
+
     phone = message.contact.phone_number
     BotDB.add_record_phone(message.from_user.id, phone)
-    # Завершаем машину состояний
     markup = ReplyKeyboardRemove()
     await state.finish()
-    # Отправляем в чат сообщение о выходе из машины состояний
     await message.answer(text='Спасибо! Вы завершили регистрацию!\n\n'
                                           'Вскоре мы подберем Вам попутчиков',reply_markup=markup)
-    # Отправляем в чат сообщение с предложением посмотреть свою анкету
     await message.answer(text='Спасибо!)')
 
 
-# # Этот хэндлер будет срабатывать на отправку команды /showdata
-# # и отправлять в чат данные анкеты, либо сообщение об отсутствии данных
-# async def process_showdata_command(message: Message):
-#     # Отправляем пользователю анкету, если она есть в "базе данных"
-#     if message.from_user.id in user_dict:
-#         await message.answer_photo(
-#             photo=user_dict[message.from_user.id]['photo_id'],
-#             caption=f'Имя: {user_dict[message.from_user.id]["name"]}\n'
-#                     f'Статус: {user_dict[message.from_user.id]["driverorpass"]}\n'
-#                     f'Место отправки: {user_dict[message.from_user.id]["start_point"]}\n'
-#                     f'Место работы: {user_dict[message.from_user.id]["end_point"]}\n'
-#                     f'Номер телефона: {user_dict[message.from_user.id]["phone"]}')
-#     else:
-#         # Если анкеты пользователя в базе нет - предлагаем заполнить
-#         await message.answer(text='Вы еще не заполняли анкету. '
-#                                   'Чтобы приступить - отправьте '
-#                                   'команду /fillform')
+
 
 
 # Этот хэндлер будет срабатывать на команду "/cancel"
@@ -212,12 +164,9 @@ async def process_cancel_command(message: Message, state: FSMContext):
     await message.answer(text='Вы вышли из регистрации\n\n'
                               'Чтобы снова перейти к регистрации - '
                               'отправьте команду /fillform')
-    # Сбрасываем состояние
     await state.reset_state()
 
 
-# Этот хэндлер будет срабатывать на любые сообщения, кроме тех
-# для которых есть отдельные хэндлеры, вне состояний
 async def send_echo(message: Message):
     await message.reply(text='Извините, следуйте инструкциям по регистрации')
 
@@ -230,8 +179,6 @@ dp.register_message_handler(process_fillform_command,
 dp.register_message_handler(process_cancel_command,
                             commands='cancel',
                             state='*')
-# dp.register_message_handler(process_showdata_command,
-#                             commands='showdata')
 
 dp.register_message_handler(process_name_sent,
                             lambda x: x.text.isalpha(),
@@ -258,9 +205,7 @@ dp.register_message_handler(process_endpoint_sent,
 dp.register_message_handler(procces_phonenumber_sent,
                             content_types='any',
                             state=FSMFillForm.fill_phone)
-#dp.register_message_handler(warning_not_phone,
- #                           content_types='any',
- #                           state=FSMFillForm.fill_phone)
+
 dp.register_message_handler(send_echo, content_types='any')
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
---
+++ src/main_test.py
@@ -0,0 +1,182 @@
+from registration_handlers import *
+
+BOT_TOKEN = '5140163343:AAGaFLxhYrbFMaZ0aV0SRxHNgpJ4J3ld6EE'
+storage: MemoryStorage = MemoryStorage()
+bot: Bot = Bot(BOT_TOKEN)
+dp: Dispatcher = Dispatcher(bot, storage=storage)
+
+
+@dp.message_handler(commands=['start'])
+async def answer(message: types.Message):
+    text = 'Поехали Вместе - это бот для поиска попутчиков по пути на работу.' \
+           ' {Далее идет краткое описание работы бота}.' \
+           ' Для использования бота нужно заполнить анкету. Заполнить сейчас?'
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    buttons = ["Нет, в другой раз...", "Заполнить"]
+    keyboard.add(*buttons)
+    await message.answer(text, reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals="Заполнить"))
+async def answer(message: types.Message):
+    text = 'Отлично! Скажите, вы регистрируетесь как Водитель или Пассажир?'
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    buttons = ["Водитель", "Пассажир"]
+    keyboard.add(*buttons)
+    await message.reply(text, reply_markup=keyboard)
+    await StateRegistration.choosing_role.set()
+
+
+@dp.message_handler(Text(equals='Нет, в другой раз...'))
+async def answer(message: types.Message):
+    await message.reply("Ok)", reply_markup=types.ReplyKeyboardRemove())
+
+
+class StateRegistration(StatesGroup):
+    choosing_role = State()
+    choosing_name = State()
+    choosing_phone = State()
+    choosing_from_location = State()
+    choosing_to_location = State()
+
+
+@dp.message_handler(Text(equals="Водитель"), state=StateRegistration.choosing_role)
+async def message_handler(message: types.Message, state: FSMContext):
+    # db.add_driver(tg_id)
+    await state.finish()
+
+    await message.reply("Укажите Ваше имя - как к Вам можно обращаться?", reply_markup=types.ReplyKeyboardRemove())
+    await StateRegistration.choosing_name.set()
+
+
+@dp.message_handler(Text(equals="Пассажир"), state=StateRegistration.choosing_role)
+async def message_handler(message: types.Message, state: FSMContext):
+    tg_id = message.from_user.id
+    # db.add_passenger(tg_id)
+    await state.finish()
+
+    await message.reply("Укажите Ваше имя - как к Вам можно обращаться?", reply_markup=types.ReplyKeyboardRemove())
+    await StateRegistration.choosing_name.set()
+
+
+@dp.message_handler(state=StateRegistration.choosing_name)
+async def message_handler(message: types.Message, state: FSMContext):
+    try:
+        tg_id = message.from_user.id
+        name = message.text
+        # db.add_name(tg_id, name)
+        await state.finish()
+
+        text = "Напишите или поделитесь привязанным к аккаунту телеграмма номером телефона"
+        keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        phone_number_button = KeyboardButton(text='Нажмите, чтобы поделиться номером телефона',
+                                             request_contact=True)
+        keyboard.add(phone_number_button)
+        await message.answer(text, reply_markup=keyboard)
+        await StateRegistration.choosing_phone.set()
+    except Exception:
+        await message.reply("Что-то пошло не так...", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text(equals="Да"), state=StateRegistration.choosing_phone)
+async def message_handler(message: types.Message, state: FSMContext):
+    print("Right address2")
+    tg_id = message.from_user.id
+    point = "point"
+    # db.writeFromPointForPassenger(tg_id, point)
+    await state.finish()
+
+    text = "Отлично, теперь укажите откуда вы едете на работу"
+    await message.reply(text, reply_markup=types.ReplyKeyboardRemove())
+    await StateRegistration.choosing_from_location.set()
+
+
+@dp.message_handler(Text(equals="Нет"), state=StateRegistration.choosing_phone)
+async def message_handler(message: types.Message):
+    print("Wrong phone")
+    await message.reply("Попробуйте указать еще раз", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=StateRegistration.choosing_phone, content_types='any')
+async def message_handler(message: types.Message, state: FSMContext):
+    try:
+        tg_id = message.from_user.id
+        if message.text is not None:
+            phone = message.text
+        else:
+            phone = message.contact.phone_number
+        # db.add_phone(tg_id, name)
+        text = phone + " - Ваш номер телефона?"
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        buttons = ["Нет", "Да"]
+        keyboard.add(*buttons)
+        await message.reply(text, reply_markup=keyboard)
+    except Exception:
+        await message.reply("Что-то пошло не так...", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text(equals="Да"), state=StateRegistration.choosing_from_location)
+async def message_handler(message: types.Message, state: FSMContext):
+    print("Right address2")
+    tg_id = message.from_user.id
+    point = "point"
+    # db.writeFromPointForPassenger(tg_id, point)
+    await state.finish()
+
+    text = "Отлично, теперь укажите куда вы едете на работу"
+    await message.reply(text, reply_markup=types.ReplyKeyboardRemove())
+    await StateRegistration.choosing_to_location.set()
+
+
+@dp.message_handler(Text(equals="Нет"), state=StateRegistration.choosing_from_location)
+async def message_handler(message: types.Message):
+    print("Wrong address2")
+    await message.reply("Попробуйте указать еще раз", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=StateRegistration.choosing_from_location)
+async def message_handler(message: types.Message, state: FSMContext):
+    try:
+        # Записываем и показываем
+        text = "{address}"
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        buttons = ["Нет", "Да"]
+        keyboard.add(*buttons)
+        await message.reply(text + " - правильный адрес?", reply_markup=keyboard)
+    except Exception:
+        await message.reply("Что-то пошло не так...", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text(equals="Да"), state=StateRegistration.choosing_to_location)
+async def message_handler(message: types.Message, state: FSMContext):
+    print("Right address")
+    tg_id = message.from_user.id
+    point = "point"
+    # db.writeFromPointForPassenger(tg_id, point)
+    await state.finish()
+
+    text = "Спасибо за регистрацию!"
+    await message.reply(text, reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(Text(equals="Нет"), state=StateRegistration.choosing_to_location.state)
+async def message_handler(message: types.Message):
+    print("Wrong address")
+    await message.reply("Попробуйте указать еще раз", reply_markup=types.ReplyKeyboardRemove())
+
+
+@dp.message_handler(state=StateRegistration.choosing_to_location.state)
+async def message_handler(message: types.Message, state: FSMContext):
+    try:
+        # Записываем и показываем
+        text = "{address}"
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+        buttons = ["Нет", "Да"]
+        keyboard.add(*buttons)
+        await message.reply(text + " - правильный адрес?", reply_markup=keyboard)
+    except Exception:
+        await message.reply("Что-то пошло не так...", reply_markup=types.ReplyKeyboardRemove())
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
